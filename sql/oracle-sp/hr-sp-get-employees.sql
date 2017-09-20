28de326a6172b46086fa14e1322e06a4d4368521
moonwave <moonwave.systems@gmail.com>
2017-09-20 12:08:03 -0700

    sp - cursor

---
+++ sql/oracle-sp/hr-sp-get-employees.sql
@@ -0,0 +1,57 @@
+-- https://stackoverflow.com/questions/10024771/calling-a-stored-procedure-in-oracle-with-in-and-out-parameters
+-- plsql stored procedure cursor example
+-- pl sql stored procedure cursor out parameter
+-- 09/20/17
+set serveroutput on;
+CREATE OR REPLACE PROCEDURE SP_GET_EMPLOYEES(V_EMPID IN NUMBER, V_OUT OUT SYS_REFCURSOR)
+AS
+BEGIN 
+  OPEN V_OUT FOR
+--  SELECT * FROM EMPLOYEES WHERE EMPLOYEE_ID = V_EMPID; -- okay
+--  SELECT * FROM EMPLOYEES WHERE EMPLOYEE_ID in (V_EMPID); -- okay
+-- 'SELECT FIRST_NAME, LAST_NAME, EMPLOYEE_id FROM EMPLOYEES WHERE EMPLOYEE_ID = V_EMPID'; -- runtime error: 00904. 00000 -  "%s: invalid identifier"
+--  SELECT FIRST_NAME, LAST_NAME FROM EMPLOYEES WHERE EMPLOYEE_ID = V_EMPID;
+  SELECT FIRST_NAME, LAST_NAME, EMPLOYEE_id FROM EMPLOYEES;
+  
+END; 
+
+
+CREATE OR REPLACE PROCEDURE SP_GET_EMPLOYEES_2(V_EMPID IN EMPLOYEES.EMPLOYEE_ID%type, V_OUT OUT SYS_REFCURSOR)
+AS
+BEGIN 
+  OPEN V_OUT FOR
+    SELECT FIRST_NAME, LAST_NAME, EMPLOYEE_id FROM EMPLOYEES;
+END;
+
+-- https://stackoverflow.com/questions/6781916/how-to-test-an-oracle-stored-procedure-with-refcursor-return-type
+DECLARE
+    v_OUT SYS_REFCURSOR; -- case insensitive for variables, must match SP IN and OUT paramters by name 
+    V_EMPID NUMBER(6,0); -- or NUMBER
+    f_name VARCHAR2(20);
+    l_name VARCHAR2(20);
+
+BEGIN
+    V_EMPID:=206;
+    SP_GET_EMPLOYEES(V_EMPID => V_EMPID, V_OUT => V_OUT);
+    LOOP
+        FETCH V_OUT INTO f_name, L_name, V_EMPID;
+        EXIT WHEN V_OUT%NOTFOUND;
+        dbms_output.put_line(V_EMPID || ' ' || f_name || ' - ' || l_name);
+    END LOOP;
+END;
+
+DECLARE
+    v_OUT SYS_REFCURSOR;
+    V_EMPID NUMBER(6,0); -- or NUMBER
+    f_name VARCHAR2(20);
+    l_name VARCHAR2(20);
+
+BEGIN
+    V_EMPID := 101;
+    SP_GET_EMPLOYEES_2(V_EMPID => V_EMPID, V_OUT => V_OUT);
+    LOOP
+        FETCH V_OUT INTO f_name, L_name, V_EMPID;
+        EXIT WHEN V_OUT%NOTFOUND;
+        dbms_output.put_line(V_EMPID || ' ' || f_name || ' - ' || l_name);
+    END LOOP;
+END;

6c903984d5a775a5fca2d9d1c3afaf09551ee224
moonwave <moonwave.systems@gmail.com>
2017-10-03 21:55:32 -0700

    virtual function impl

--- c++/src/virtual-functions.cpp
+++ c++/src/virtual-functions.cpp
@@ -31,16 +31,15 @@ class Professor : public Person {
     int publications;
     int cur_id;
 public:
-    Professor (string name, int age, int publications, int cur_id) : Person(name, age), publications(publications), cur_id(cur_id) {
+    Professor (string name, int age, int cur_id) : Person(name, age), cur_id(cur_id) {
     }
     void getdata() {
-        Person::getdata();
         cin >> publications;
     }
     void putdata() {
         printf("%s %d %d %d\n", name.c_str(), age, publications, cur_id);
     }
-    
+
 };
 
 class Student : public Person {
@@ -50,7 +49,6 @@ public:
     Student (string name, int age, int cur_id) : Person(name, age), cur_id(cur_id) {
     }
     void getdata() {
-        Person::getdata();
         for (int i = 0; i < 6; i++) {
             cin >> marks[i];
         }
@@ -64,6 +62,45 @@ public:
     }
 };
 
+/*
+Input:
+4
+1
+Walter 56 99
+2
+Jesse 18 50 48 97 76 34 98
+2
+Pinkman 22 10 12 0 18 45 50
+1
+White 58 87
+
+Output:
+Walter 56 99 1
+Jesse 18 403 1
+Pinkman 22 135 2
+White 58 87 2
+*/
 int main() {
+    int n;
+    cin >> n;
+    int professor_id = 1;
+    int student_id = 1;
+    while (n-- > 0) {
+        int type;
+        string name;
+        int age;
+        cin >> type;
+        cin >> name;
+        cin >> age;
+        if (type == 1) {
+            Person* p = new Professor(name, age, professor_id++);
+            p->getdata();
+            p->putdata();
+        } else {
+            Person* p = new Student(name, age, student_id++);
+            p->getdata();
+            p->putdata();
+        }
+    }
     return 0;
 }

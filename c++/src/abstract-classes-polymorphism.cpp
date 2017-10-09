6edcf7a5ce63c4312a8c53c61c5f2cb3fcc2e8c0
moonwave <moonwave.systems@gmail.com>
2017-10-08 20:29:05 -0700

    LRU cache impl

--- c++/src/abstract-classes-polymorphism.cpp
+++ c++/src/abstract-classes-polymorphism.cpp
@@ -3,6 +3,7 @@
  * 10/07/17
  */
 #include <iostream>
+#include <cstdio>
 #include <vector>
 #include <map>
 #include <string>
@@ -21,56 +22,112 @@ struct Node{
 };
 
 class Cache{
-   
-   protected: 
-   map<int,Node*> mp; //map the key to the node in the linked list
-   int cp;  //capacity
-   // head and tail is the cache data structure
-   Node* tail; // double linked list tail pointer
-   Node* head; // double linked list head pointer
-   public:
-   virtual void set(int, int) = 0; //set function
-   virtual int get(int) = 0; //get function
+
+    protected: 
+    map<int,Node*> mp; //map the key to the node in the linked list
+    int cp;  //capacity
+    // head and tail is the cache data structure
+    Node* tail; // double linked list tail pointer
+    Node* head; // double linked list head pointer
+    virtual void set(int, int) = 0; //set function
+    virtual int get(int) = 0; //get function
 
 };
+
 // http://www.cplusplus.com/reference/map/map/find/
 class LRUCache : public Cache {
+    int count;
     public:
-    LRUCache(int c) {
+    LRUCache(int c) : count(0) {
         cp = c;
+        head = NULL;
+        tail = NULL;
     };
     virtual void set(int key, int value);
     virtual int get(int);
+    void printCache();
 };
+
+void LRUCache::printCache() {
+    Node* curr = head;
+    while (curr != NULL) {
+//    	printf("(%d, %d) ", curr->key, curr->value);
+        curr = curr->next;
+    }
+}
 
 void LRUCache::set(int key, int value) {
     std::map<int, Node*>::iterator it;
     it = mp.find(key);
     if (it != mp.end()) { // found
-        return; // does nothing, no duplicate keys allowed
+    	Node* curr = (*it).second;
+    	curr->value = value;
+        return; // replace with new value for the same key
     }
     Node* node = new Node(key, value);
-    if (head == NULL) { // insert the first node
-        head = node;
-        tail = node;
-        mp.insert(std::pair<int, Node*>(key, node));
-        return;
-    }
-    // add at the tail
-    tail->next = node;
-    tail = node;
     mp.insert(std::pair<int, Node*>(key, node));
 }
 
 int LRUCache::get(int key) {
+    // lookup from cache first
+    Node* curr = head;
+    while (curr != NULL) {
+        if (curr->key == key) {
+            if (curr == head) {
+                // key is the first one, does nothing
+                return curr->value;
+            } else if (curr == tail) {
+            	if (curr->prev != NULL) {
+					tail = curr->prev;
+					tail->next = NULL;
+            	}
+                // move to the head
+                curr->next = head->next;
+                head = curr;
+                head->prev = NULL;
+            } else { // move recently used at head
+                curr->prev->next = curr->next;
+                curr->next = head->next;
+                head = curr;
+                head->prev = NULL;
+            }
+            return curr->value;
+        }
+        curr = curr->next;
+    }
+    // search from map
     std::map<int, Node*>::iterator it;
     it = mp.find(key);
-    if (it != mp.end()) { // found
-        std::pair<int, Node*> pair = *it;
-        return pair.second->value; 
-    } else {
+    if (it == mp.end()) { // not found
         return -1;
     }
+    std::pair<int, Node*> pair = *it;
+    curr = pair.second;
+    // add the node just found at the head
+    // remove LRU from tail if capacity exceeded
+    count++;
+    if (head == NULL) { // insert the first node
+        head = curr;
+        tail = curr;
+        head->next = NULL;
+        head->prev = NULL;
+    } else { // add at head
+        curr->next = head;
+        head->prev = curr;
+        head = curr;
+        head->prev = NULL;
+        if (count > cp) {
+            // rermove the LRU node at tail
+        	if (tail->prev != NULL) {
+				tail = tail->prev;
+				tail->next = NULL;
+        	} else {
+        		tail = head;
+        	}
+            count = cp;
+        }
+    }
+    return curr->value; 
 }
 
 int main() {
@@ -84,6 +141,7 @@ int main() {
          int key;
          cin >> key;
          cout << l.get(key) << endl;
+         l.printCache();
       } 
       else if(command == "set") {
          int key, value;
@@ -93,3 +151,47 @@ int main() {
    }
    return 0;
 }
+
+/*
+3 1
+set 1 2
+get 1
+get 2
+
+2
+-1
+-- =======================
+4 1
+set 1 2
+set 2 7
+get 1
+get 2
+
+2
+-1
+-- =======================
+6 2
+set 1 2
+set 2 7
+get 1
+get 2
+set 2 9
+get 2
+
+
+-- =======================
+8 4
+set 4 2
+set 2 7
+get 2
+set 1 8
+set 5 9
+set 6 15
+get 4
+get 5
+
+Expected output:
+7
+-1
+9
+*/

6fb65311ca5f83366c8b9e879e9687aabd67c445
moonwave <moonwave.systems@gmail.com>
2017-10-19 00:14:47 -0700

    heap 2

--- c++/src/ds/heap1.cpp
+++ c++/src/ds/heap1.cpp
@@ -18,6 +18,35 @@
 #include <algorithm>
 using namespace std;
 
+/*
+	http://www.cplusplus.com/forum/beginner/158390/
+	auto it = std::lower_bound(v.begin(), v.end(), num);
+	if(it == v.end() || *it != num) {
+	    std::cout << num << " not found\n"
+	} else {
+	    std::size_t index = std::distance(v.begin(), it)
+	    std::cout << num << " was found at " << index << '\n';
+	    std::cout << "Replacing with 0";
+	    *it = 0; // v[index] = 0
+	}
+*/
+/**
+ * https://stackoverflow.com/questions/27431029/binary-search-with-returned-index-in-stl
+ *
+ * @param v - sorted vector instance
+ * @param data - value to search
+ * @return 0-based index if data found, -1 otherwise
+*/
+int binary_search_find_index(std::vector<int> v, int data) {
+    auto it = std::lower_bound(v.begin(), v.end(), data);
+    if (it == v.end() || *it != data) {
+        return -1;
+    } else {
+        std::size_t index = std::distance(v.begin(), it);
+        return index;
+    }   
+}
+
 // http://www.sanfoundry.com/cpp-program-create-heap-using-make_heap-operation/
 void print(const std::vector <int>& v) {
     std::vector <int>::const_iterator i;
@@ -29,9 +58,9 @@ void print(const std::vector <int>& v) {
 
 // https://stackoverflow.com/questions/14016921/comparator-for-min-heap-in-c
 struct greater_func { // use greater function to make a min heap
-  bool operator()(const long& a,const long& b) const{
-    return a>b;
-  }
+	bool operator()(const long& a,const long& b) const{
+		return a>b;
+	}
 };
 
 int main() {
@@ -49,19 +78,22 @@ int main() {
     		std::make_heap(v.begin(), v.end(), greater_func());
     	} else if (type == 2) { // "2 v" - Delete the element from the heap
     		cin >> data;
-    		int size = v.size();
-    		while (size-- > 0) {    	
-	    		std::pop_heap(v.begin(), v.end(), greater_func()); // pop action moves the "root" to the end of vector 
-	    		if (data == v[v.size() - 1]) {
-	    			v.pop_back(); // remove the data
-	    		}
-    		}
+    		
+    		std::sort (v.begin(), v.end());
+    		std::vector<int>::iterator low;
+  			int index = binary_search_find_index(v, data);
+  			if (index != -1) {
+	  			std::swap(v[index], v[v.size() - 1]);
+	  			v.pop_back();
+	    		std::make_heap(v.begin(), v.end(), greater_func());
+  			}
     	} else if (type == 3) { // "3" - Print the minimum of all the elements in the heap
     		cout << v.front() << endl;
     	}
     }
     return 0;
 }
+
 /*
 5
 1 4  
